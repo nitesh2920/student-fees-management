7b99ab672c443e6ff4f80f3fc04a8304fbfc9185
nitesh2920 <niteshtiwari0234@gmail.com>
2025-07-18 03:59:46 +0530

    correction

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,12 +1,9 @@
-// src/lib/axios.ts
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Change this to your backend URL
-   // if using cookies for auth (optional)
+  baseURL: "http://localhost:5000/api", 
 });
 
-// Add interceptors if needed (optional)
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
